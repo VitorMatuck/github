f35f4af0e7ceff969cda9756babeb61d8d0a5a64
VitorMatuck <vitormatuck@hotmail.com>
2018-09-20 20:32:28 -0300

    Fala comigo bb

---
+++ Rock/mengo/__init__.py
@@ -0,0 +1,8 @@
+import math
+
+px1= int(input("Coordenada de px1: "))
+px2= int(input("Coordenada de px2: "))
+py1= int(input("Coordenada de py1: "))
+py2= int(input("Coordenada de py2: "))
+d=math.sqrt ((px1-px2)**2+(py1-py2)**2)
+print("distancia entre os pontos",d)
