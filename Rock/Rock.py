379eca7d84be478e898be8217e2e9c70ef2f89fe
VitorMatuck <vitormatuck@hotmail.com>
2018-09-20 20:11:39 -0300

    Fala comigo bb

--- Rock/Rock.py
+++ Rock/Rock.py
@@ -2,4 +2,21 @@ print("oi, mundo")
 
 print("fala comigo bb")
 
+import math
+a = int(input("Informe valor de a: "))
+b = int(input("Informe valor de b: "))
+c = int(input("Informe valor de c: "))
+
+delta = b * b - 4 * a * c
+
+print("Delta: ", delta)
+
+if delta >= 0:
+    x1 = (-b + math.sqrt(delta)) / (2* a)
+    x2 = (-b - math.sqrt(delta)) / (2 * a)
+    print("Raiz x1: ", x1)
+    print("Raiz x2: ", x2)
+else:
+    print("Não existem raízes reais")
+
 
